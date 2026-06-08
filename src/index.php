0627dd18834961198204f5f2a9c9e9f97ed484bc
salim-nejdi <nejdi.salim@gmail.com>
2026-06-08 15:55:14 +0200

    feat(ci): add canary5

--- src/index.php
+++ src/index.php
@@ -1,6 +1,5 @@
 <?php
 // =============================================================================
-// SECTION 1 — CONFIGURATION
 // On lit les variables d'environnement injectées par Kubernetes (Secret K8s)
 // ou par Docker en local (.env via docker-compose).
 // getenv('VAR') retourne false si la variable n'existe pas.
